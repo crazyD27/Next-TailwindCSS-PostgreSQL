d6f96059c240b5a81b85ea61fd959963f25e75eb
Roger-Davis <rd3328425@gmail.com>
2023-06-29 15:25:56 -0300

    feat: creating Footer component and adding styles.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className="flex w-full items-center justify-center bg-gray-950">
+      <div className="flex w-full max-w-[1300px] items-center justify-between p-3 ">
+        <ul className="flex list-none flex-col gap-2">
+          <li className="font-body text-sm text-zinc-50 hover:text-zinc-200">
+            <Link href="/">Página inicial</Link>
+          </li>
+          <li className="font-body text-sm text-zinc-50 hover:text-zinc-200">
+            <Link href="/">Sobre</Link>
+          </li>
+        </ul>
+        <ul className="flex list-none items-center">
+          <li>
+            <a
+              href="https://github.com/iarlen-reis"
+              rel="noreferrer"
+              target="_blank"
+              className="font-body italic text-zinc-50 hover:text-zinc-200"
+            >
+              Feito por Iarlen Reis
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  )
+}
