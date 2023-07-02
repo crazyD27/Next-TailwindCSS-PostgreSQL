d69e230e03019616283c75a5dad7142a35f4e750
Roger-Davis <rd3328425@gmail.com>
2023-07-02 19:08:48 -0300

    feat: adding NextAuth provider in Providers component.

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,11 +1,23 @@
 'use client'
-import { AuthProvider } from '@/contexts/authContext'
 import React, { ReactNode } from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { AuthProvider } from '@/contexts/authContext'
+import { SessionProvider } from 'next-auth/react'
+import { Session } from 'next-auth'
 
 interface IProvidersProps {
   children: ReactNode
+  session?: Session
 }
 
-export const Providers = ({ children }: IProvidersProps) => {
-  return <AuthProvider>{children}</AuthProvider>
+export const Providers = ({ children, session }: IProvidersProps) => {
+  const queryclient = new QueryClient()
+
+  return (
+    <SessionProvider session={session}>
+      <QueryClientProvider client={queryclient}>
+        <AuthProvider>{children}</AuthProvider>
+      </QueryClientProvider>
+    </SessionProvider>
+  )
 }
