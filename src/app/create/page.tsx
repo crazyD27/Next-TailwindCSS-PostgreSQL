025410089b5f272cf596522e7fe1acf5101d8a3e
Roger-Davis <rd3328425@gmail.com>
2023-07-05 19:06:45 -0300

    feat: creating the Create page and adding the CreateTaskForm component.

---
+++ src/app/create/page.tsx
@@ -0,0 +1,22 @@
+import { CreateTaskForm } from '@/components/Forms/CreateTaskForm'
+import React from 'react'
+
+const Create = () => {
+  return (
+    <div className="m-auto max-w-fit py-4 pb-10">
+      <div className="m-auto flex w-full max-w-[450px] flex-col gap-4 rounded-md border border-zinc-400 p-3 sm:px-5 lg:px-6">
+        <div className="flex flex-col">
+          <h1 className="font-title text-xl sm:text-2xl lg:text-3xl">
+            Criar tarefa
+          </h1>
+          <p className="font-body text-xs italic sm:text-sm">
+            Crie sua tarefa de forma fácil.
+          </p>
+        </div>
+        <CreateTaskForm />
+      </div>
+    </div>
+  )
+}
+
+export default Create
