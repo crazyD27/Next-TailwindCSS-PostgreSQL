5e8c623ce0112c7b6ed79fc625795c7c91906064
Roger-Davis <rd3328425@gmail.com>
2023-07-05 02:21:40 -0300

    feat: finalizing structure of Header component, changing background and adding Logout and Profile button.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,56 @@
 import Link from 'next/link'
-import React from 'react'
+import Image from 'next/image'
+import UserDefault from '/public/user-default.png'
+import { useGetUser } from '@/hooks/useGetUser'
+import LogoutButton from './LogoutButton'
 
-export const Header = () => {
+export const Header = async () => {
+  const { session } = await useGetUser()
+
   return (
-    <header className="flex w-full items-center justify-center bg-zinc-600">
+    <header className="flex h-20 w-full items-center justify-center border-b border-red-200 bg-zinc-900">
       <nav className="flex w-full max-w-[1300px] items-center justify-between p-3">
         <Link
           href="/"
-          className="font-title text-2xl uppercase text-zinc-50 hover:text-zinc-200"
+          className="font-title text-2xl text-zinc-50 hover:text-zinc-200"
         >
-          Todo
+          NextTodo
         </Link>
-        <ul className="flex list-none items-center gap-3">
-          <li className="font-body text-base text-zinc-50 hover:text-zinc-200">
-            <Link href="/login">Entrar</Link>
-          </li>
-          <li className="font-body text-base text-zinc-50 hover:text-zinc-200">
-            <Link href="/register">Cadastrar</Link>
-          </li>
+        <ul className="flex list-none items-center gap-5">
+          {!session ? (
+            <li>
+              <Link href="/login" className="text-zinc-50">
+                Login
+              </Link>
+            </li>
+          ) : (
+            <>
+              <li>
+                <Link href="/profile">
+                  {session.user?.image ? (
+                    <Image
+                      src={session.user.image}
+                      alt={`Foto de perfil do usuário ${session.user?.name}`}
+                      width={35}
+                      height={35}
+                      className="rounded-full transition-transform hover:scale-[1.1]"
+                    />
+                  ) : (
+                    <Image
+                      src={UserDefault}
+                      alt={`Foto de perfil do usuário ${session.user?.name}`}
+                      width={35}
+                      height={35}
+                      className="rounded-full"
+                    />
+                  )}
+                </Link>
+              </li>
+              <li>
+                <LogoutButton />
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
