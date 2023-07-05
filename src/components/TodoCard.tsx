9d59967caf0b7b509d091c19d97264a5c3183dbf
Roger-Davis <rd3328425@gmail.com>
2023-07-05 18:06:17 -0300

    styles: changing styles of the TodoCard component.

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,33 +1,39 @@
 import Link from 'next/link'
 import React from 'react'
-import { Star } from 'lucide-react'
+import { Star, ArrowUpRight } from 'lucide-react'
 
 export const TodoCard = () => {
   return (
-    <div className="m-auto w-full max-w-[340px] rounded-md bg-zinc-600 p-3">
+    <div className="m-auto w-full max-w-[340px] rounded-md bg-blue-300 p-3 shadow shadow-pink-300 odd:bg-pink-300 odd:shadow-blue-300">
       <div className="flex flex-col gap-2">
         <div className="flex w-full items-center justify-between">
-          <span className="font-title text-lg text-zinc-50">
-            Tosar Doguinho
-          </span>
-          <Link href="/">
-            <Star size={18} color="#FFF" />
-          </Link>
-        </div>
-        <div className="flex w-full flex-col gap-1">
-          <span className="font-body text-sm italic text-zinc-50">
+          <span className="w-fit rounded-full bg-zinc-100 px-3 py-1 font-body text-sm font-medium italic text-zinc-900">
             11/10/2023
           </span>
-          <span className="font-body text-sm italic text-zinc-50">
+          <Link href="/">
+            <Star
+              size={18}
+              className="text-zinc-900 transition-colors hover:text-zinc-600"
+            />
+          </Link>
+        </div>
+        <div className="flex flex-col gap-2">
+          <span className="font-title text-xl text-zinc-900">
+            Tosar Doguinho
+          </span>
+          <span className="w-fit rounded-full border border-zinc-600 px-2 py-1 font-body text-xs font-medium italic text-zinc-900">
             Pendente
           </span>
         </div>
-        <Link
-          href="/"
-          className="flex items-center justify-end self-end rounded-md bg-zinc-700 px-3 py-2 font-body text-sm text-zinc-50"
-        >
-          Detalhes
-        </Link>
+        <button className="group flex items-center justify-center gap-1 self-end rounded-md border border-zinc-600 px-2 py-1 transition-colors hover:bg-zinc-950">
+          <ArrowUpRight
+            size={20}
+            className="text-zinc-800 group-hover:text-zinc-50"
+          />
+          <span className="text-base text-zinc-800 group-hover:text-zinc-50">
+            Detalhes
+          </span>
+        </button>
       </div>
     </div>
   )
