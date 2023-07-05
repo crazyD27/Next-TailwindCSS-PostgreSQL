d515d0672565d94fb79cd71a8573c22c800c0ac6
Roger-Davis <rd3328425@gmail.com>
2023-07-05 02:22:54 -0300

    styles: adding responsiveness on the Home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <h1 className="font-body">Hello Word!</h1>
+import { MenuTools } from '@/components/MenuTools'
+import { TodoCard } from '@/components/TodoCard'
+
+export default async function Home() {
+  return (
+    <div className="mt-3 flex w-full flex-col justify-center gap-6">
+      <MenuTools />
+      <div className="grid w-full grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <TodoCard />
+        <TodoCard />
+        <TodoCard />
+        <TodoCard />
+      </div>
+    </div>
+  )
 }
