7d62991ddcc318034b0210cb2ca32f12051dfb89
Roger-Davis <rd3328425@gmail.com>
2023-07-06 01:06:32 -0300

    feat: creating routes create and fetch tasks.

---
+++ src/app/api/tasks/route.ts
@@ -0,0 +1,57 @@
+import { prisma } from '@/utils/prisma'
+import { NextRequest, NextResponse } from 'next/server'
+import { useGetUser } from '@/hooks/useGetUser'
+import z from 'zod'
+
+export async function GET(request: NextRequest) {
+  const { session } = await useGetUser()
+
+  if (!session?.user) {
+    return NextResponse.json(
+      { error: 'Essa rota precisa de autenticação.' },
+      { status: 401 },
+    )
+  }
+
+  const tasks = await prisma.task.findMany({
+    where: {
+      userId: session.user.id,
+    },
+  })
+
+  return NextResponse.json({ data: tasks })
+}
+
+export async function POST(request: NextRequest) {
+  const { session } = await useGetUser()
+
+  if (!session?.user) {
+    return NextResponse.json(
+      { error: 'Essa rota precisa de autenticação.' },
+      { status: 401 },
+    )
+  }
+
+  const taskShema = z.object({
+    title: z.string(),
+    dateConclusion: z.string(),
+    description: z.string(),
+  })
+
+  const { title, dateConclusion, description } = taskShema.parse(
+    await request.json(),
+  )
+
+  await prisma.task.create({
+    data: {
+      title,
+      dateConclusion,
+      description,
+      userId: session.user.id as string,
+    },
+  })
+
+  return NextResponse.json({}, { status: 201 })
+}
+
+export async function DELETE(request: NextRequest) {}
