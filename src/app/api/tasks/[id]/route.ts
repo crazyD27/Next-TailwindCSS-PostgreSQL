a19a8e350ab02f7ac7c32965d32ebb088609305a
Roger-Davis <rd3328425@gmail.com>
2023-07-06 23:31:30 -0300

    refactor: removing routes from fetch all tasks, removing route from fetching a task and removing session checking from DELETE and PUT routes.

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -9,45 +9,6 @@ interface IParamsProps {
   }
 }
 
-export async function GET(request: NextRequest, { params }: IParamsProps) {
-  const { session } = await useGetUser()
-
-  const taskShema = z.object({
-    id: z.string(),
-  })
-
-  const { id } = taskShema.parse(params)
-
-  if (!session) {
-    return NextResponse.json(
-      { error: 'Essa rota precisa de autenticação.' },
-      { status: 401 },
-    )
-  }
-
-  const task = await prisma.task.findUnique({
-    where: {
-      id,
-    },
-  })
-
-  if (!task) {
-    return NextResponse.json(
-      { error: 'Tarefa não encontrada.' },
-      { status: 404 },
-    )
-  }
-
-  if (task.userId !== session.user.id) {
-    return NextResponse.json(
-      { error: 'Operação não permitida.' },
-      { status: 401 },
-    )
-  }
-
-  return NextResponse.json(task, { status: 200 })
-}
-
 export async function DELETE(request: NextRequest, { params }: IParamsProps) {
   const { session } = await useGetUser()
 
@@ -74,16 +35,47 @@ export async function DELETE(request: NextRequest, { params }: IParamsProps) {
     )
   }
 
-  if (task.id !== session.user.id) {
-    return NextResponse.json(
-      { error: 'Operação não permitida.' },
-      { status: 401 },
-    )
-  }
-
   await prisma.task.delete({
     where: {
       id,
+    },
+  })
+
+  return NextResponse.json(task, { status: 200 })
+}
+
+export async function PUT(request: NextRequest, { params }: IParamsProps) {
+  const { session } = await useGetUser()
+
+  if (!session) {
+    return NextResponse.json({ error: 'Essa rota precisa de autenticação.' })
+  }
+
+  const taskShema = z.object({
+    id: z.string(),
+  })
+
+  const { id } = taskShema.parse(params)
+
+  const task = await prisma.task.findUnique({
+    where: {
+      id,
+    },
+  })
+
+  if (!task) {
+    return NextResponse.json(
+      { error: 'Tarefa não encontrada.' },
+      { status: 404 },
+    )
+  }
+
+  await prisma.task.update({
+    where: {
+      id,
+    },
+    data: {
+      status: 'concluído',
     },
   })
 
