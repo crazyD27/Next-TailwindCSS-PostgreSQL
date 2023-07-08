2ae64e772d08f30a87352d3f7b5397745428a739
Roger-Davis <rd3328425@gmail.com>
2023-07-07 23:07:53 -0300

    refactor: transforming the individual task page into a client side, to use react query.

--- src/app/task/[id]/page.tsx
+++ src/app/task/[id]/page.tsx
@@ -1,9 +1,11 @@
+'use client'
+
 import Link from 'next/link'
 import { ChevronRight } from 'lucide-react'
 import { formateDateToShow } from '@/utils/formateDate'
 import { ButtonsTask } from '@/components/ButtonsTask'
-import { useTasks } from '@/hooks/useTasks'
-import { redirect } from 'next/navigation'
+import { useQuery } from 'react-query'
+import { api } from '@/services/api'
 
 interface IParamProps {
   params: {
@@ -11,48 +13,60 @@ interface IParamProps {
   }
 }
 
-const Task = async ({ params: { id } }: IParamProps) => {
-  const { getTask } = useTasks()
+interface ITasksProps {
+  id: string
+  title: string
+  status: string
+  dateConclusion: string
+  description: string
+  createdAt: Date
+  userId: string
+}
 
-  const task = await getTask(id)
+const Task = ({ params: { id } }: IParamProps) => {
+  const { data: task } = useQuery([id], async () => {
+    const response = await api.get<ITasksProps>(`/tasks/${id}`)
 
-  if (!task) {
-    redirect('/')
-  }
+    return response.data
+  })
 
   return (
     <div className="m-auto w-full max-w-[500px]">
-      <p className="mt-3 flex items-center justify-start gap-1 font-body text-sm text-zinc-700">
-        <Link href="/" className="font-medium hover:text-zinc-800">
-          Página inicial
-        </Link>
-        <span className="text-base">
-          <ChevronRight size={12} />
-        </span>
-        <span className="font-bold capitalize">{task.title}</span>
-      </p>
-      <div className="mt-6 flex items-center justify-between">
-        <div className="flex flex-col gap-1 sm:gap-2">
-          <h1 className="font-title text-2xl capitalize text-zinc-800 sm:text-3xl">
-            {task.title}
-          </h1>
-          <p className="font-body text-xs text-zinc-700 sm:text-sm">
-            {formateDateToShow(task.dateConclusion)} -{' '}
-            <span className="capitalize">{task.status}</span>
+      {task && (
+        <>
+          <p className="mt-3 flex items-center justify-start gap-1 font-body text-sm text-zinc-700">
+            <Link href="/" className="font-medium hover:text-zinc-800">
+              Página inicial
+            </Link>
+            <span className="text-base">
+              <ChevronRight size={12} />
+            </span>
+            <span className="font-bold capitalize">{task.title}</span>
           </p>
-        </div>
-        <div className="mt-1">
-          <ButtonsTask id={task.id} status={task.status} />
-        </div>
-      </div>
-      <div className="mt-3 flex flex-col gap-1">
-        <span className="font-body text-sm text-zinc-800 sm:text-lg">
-          Descrição:
-        </span>
-        <p className="h-[300px] rounded-md border border-zinc-400 p-2 font-body text-sm text-zinc-800">
-          {task.description}
-        </p>
-      </div>
+          <div className="mt-6 flex items-center justify-between">
+            <div className="flex flex-col gap-1 sm:gap-2">
+              <h1 className="font-title text-2xl capitalize text-zinc-800 sm:text-3xl">
+                {task.title}
+              </h1>
+              <p className="font-body text-xs text-zinc-700 sm:text-sm">
+                {formateDateToShow(task.dateConclusion)} -{' '}
+                <span className="capitalize">{task.status}</span>
+              </p>
+            </div>
+            <div className="mt-1">
+              <ButtonsTask id={task.id} status={task.status} />
+            </div>
+          </div>
+          <div className="mt-3 flex flex-col gap-1">
+            <span className="font-body text-sm text-zinc-800 sm:text-lg">
+              Descrição:
+            </span>
+            <p className="h-[300px] rounded-md border border-zinc-400 p-2 font-body text-sm text-zinc-800">
+              {task.description}
+            </p>
+          </div>
+        </>
+      )}
     </div>
   )
 }
