0b0892d86454e873b2a66ff04dd6e39257c30210
Roger-Davis <rd3328425@gmail.com>
2023-07-07 23:05:58 -0300

    refactor: transforming the Home page into client side, to use react query.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
+'use client'
 import { MenuTools } from '@/components/MenuTools'
 import { TodoCard } from '@/components/TodoCard'
 import { useTasks } from '@/hooks/useTasks'
 
-export default async function Home() {
-  const { getAllTasks } = useTasks()
-
-  const data = await getAllTasks()
+export default function Home() {
+  const { tasks } = useTasks()
 
   return (
     <div className="mt-3 flex w-full flex-col justify-center gap-6 pb-20">
       <MenuTools />
       <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {data &&
-          data.map((task) => (
+        {tasks &&
+          tasks.map((task) => (
             <TodoCard
               id={task.id}
               title={task.title}
