0bceade3da7580e9e18058acd3b16756e3112be3
Roger-Davis <rd3328425@gmail.com>
2023-07-09 04:39:07 -0300

    feat: creating functionality to choose the color of the task card and creating a list with available colors.

--- src/components/Forms/CreateTaskForm.tsx
+++ src/components/Forms/CreateTaskForm.tsx
@@ -1,17 +1,20 @@
 'use client'
 import React, { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
-
 import { useTasks } from '@/hooks/useTasks'
+import { allowedColors } from '@/utils/TasksColors'
+import { toast } from 'react-toastify'
 
 interface IFormProps {
   title: string
   dateConclusion: string
   description: string
+  color: string
 }
 
 export const CreateTaskForm = () => {
   const { createTask, loadingCreate } = useTasks()
+
   const {
     register,
     handleSubmit,
@@ -20,7 +23,12 @@ export const CreateTaskForm = () => {
   } = useForm<IFormProps>()
 
   const handleCreateTask = (data: IFormProps) => {
+    if (!allowedColors.includes(data.color)) {
+      return toast.error('Selecione uma das cores disponíveis.')
+    }
+
     createTask(data)
+    reset()
   }
 
   useEffect(() => {
@@ -44,7 +52,7 @@ export const CreateTaskForm = () => {
           <small className="text-xs text-red-400">Campo obrigatório.</small>
         )}
       </label>
-      <label className="flex flex-col gap-1">
+      <label className="flex flex-1 flex-col gap-1">
         <span className="font-body font-medium">Data de conclusão</span>
         <input
           type="date"
@@ -69,6 +77,19 @@ export const CreateTaskForm = () => {
           <small className="text-xs text-red-400">Campo obrigatório.</small>
         )}
       </label>
+      <input
+        type="color"
+        list="colorList"
+        disabled={loadingCreate}
+        className="absolute right-4 top-4 h-7 w-12 rounded bg-zinc-300 p-0.5 px-1"
+        {...register('color')}
+        defaultValue={allowedColors[0]}
+      />
+      <datalist id="colorList">
+        {allowedColors.map((color) => (
+          <option value={color} key={color} />
+        ))}
+      </datalist>
       {!loadingCreate ? (
         <button className="flex w-32 items-center justify-center self-end rounded-md bg-blue-400 p-2 text-zinc-50 transition-colors hover:bg-blue-500">
           Criar
---
+++ src/utils/TasksColors.ts
@@ -0,0 +1,12 @@
+export const allowedColors = [
+  '#f9a8d4',
+  '#93c5fd',
+  '#6ee7b7',
+  '#bef264',
+  '#fde047',
+  '#fdba74',
+  '#fca5a5',
+  '#67e8f9',
+  '#c4b5fd',
+  '#fcd34d',
+]
